c89d20ab7b7fa85197babd47431cb7a030c36065
Veniotn <w0404614@nscc.ca>
2023-11-24 18:40:17 -0400

    New force calculations made

--- Classes/Boxing_Machine.py
+++ Classes/Boxing_Machine.py
@@ -1,61 +1,84 @@
+import math
+import time
+
 class BoxingMachine:
 
     def __init__(self, controller):
         self.hx711_controller = controller
         self.session_high_score = 0
 
-    def calculate_score(self, reading, last_reading):
-        # bags mass, calculated by taking the dimensions of my speed bag to get its volume,
-        # and the density of its material. genuine leather sits between,
-        # .9 to 1.2 g/cm2, so we used 1.0 g/cm2 as an average density, converted it to cubic meters,
-        # we multiply the volume and density together to get our mass in grams and finally convert it to kgs.
-        BAG_MASS_KG = 0.023328
+        def convert_kg(data):
+            return data / 1000
 
-        # if either readings are negative, negate them
-        if reading < 0:
-            reading = -reading
-        if last_reading < 0:
-            last_reading = -last_reading
+        def calculate_score(reading, last_idle_reading, elapsed_time):
+            # bags mass, calculated by taking the dimensions of my speed bag to get its volume,
+            # and the density of its material. genuine leather sits between,
+            # .9 to 1.2 g/cm2, so we used 1.0 g/cm2 as an average density, converted it to cubic meters,
+            # we multiply the volume and density together to get our mass in grams and finally convert it to kgs.
+            BAG_MASS_KG = 0.023328
 
-        # calculate the raw punch value by finding the difference between the idle reading and punch reading
-        print("Reading: ", reading)
-        print("raw punch value: ", (reading - last_reading))
+            # if either readings are negative, negate them
+            if reading < 0:
+                reading = -reading
+            if last_idle_reading < 0:
+                last_reading = -last_idle_reading
 
-        # our reading is in grams so we convert it to kgs to stay consistent
-        reading_kg = reading / 1000
+            # our reading is in grams, so we convert it to kgs to stay consistent
+            reading = convert_kg(reading)
+            last_idle_reading = convert_kg(last_reading)
 
-        # for now we will use the current reading as acceleration (to be tweaked) and calulate the force as f = m * a
-        punch_force = BAG_MASS_KG * reading_kg
-        print("Score: ", punch_force)
+            # calculate angular velocity, the bag will travel 90 degrees before it hits the bag
+            ANGULAR_DISPLACEMENT = 90
+
+            # convert displacement degrees to radians
+            angular_displacement_rad = ANGULAR_DISPLACEMENT * (math.pi / 180)
+
+            # calculate angular velocity
+            angular_velocity = angular_displacement_rad / elapsed_time
 
 
-    def run(self):
-        # idle limit is the max value a reading can be before we register it as a valid hit
-        IDLE_LIMIT = 1000
-        # amount of readings since last valid reading
-        reading_count = 0
-        # value of the last idle reading used to find the value of our hit.
-        last_idle_reading = 0
+            # calculate acceleration as a = angular_velocity / elapsed_time
+            acceleration = angular_velocity / elapsed_time
 
-        # prompt the user to interact.
-        print("PUNCH TO GET A HIGH SCORE!")
+            # calculate force as f = m * a
+            punch_force = BAG_MASS_KG * acceleration
+            print("Score: ", punch_force)
 
-        while True:
-            # zero the load cell
-            self.hx711_controller.zero()
-            # check for a reading
-            reading = self.hx711_controller.get_data_mean()
+        def run(this):
+            # idle limit is the max value a reading can be before we register it as a valid hit
+            IDLE_LIMIT = 3000
+            # amount of readings since last valid reading
+            reading_count = 0
+            # value of the last idle reading used to find the value of our hit.
+            last_idle_reading = 0
 
-            # if the reading is greater than the idle limit in either direction, the bag has been punched.
-            if reading > IDLE_LIMIT or reading < -IDLE_LIMIT:
-                self.calculate_score(reading, last_idle_reading)
-            else:
-                # if no reading found, up the idle reading count and prompt the user
-                reading_count += 1
-                # save the current idle reading
-                last_idle_reading = reading
-                print("Waiting for punch: ", reading_count)
+            # prompt the user to interact.
+            print("PUNCH TO GET A HIGH SCORE!")
 
-            # if enough readings pass without the bag being hit, exit the program.
-            if reading_count > 3:
-                break
+            # main program loop
+            while True:
+                # zero the load cell
+                this.hx711_controller.zero()
+                # check for a reading, start our timer, used to calculate acceleration
+                start_time = time.time()
+                reading = this.hx711_controller.get_data_mean()
+
+                # if the reading is greater than the idle limit in either direction, the bag has been punched.
+                if reading > IDLE_LIMIT or reading < -IDLE_LIMIT:
+                    # calculate the elapsed time since the bag has been punched
+                    elapsed_time = time.time() - start_time
+                    # calculate our score
+                    this.calculate_score(reading, last_idle_reading, elapsed_time)
+                    # reset idle reading count
+                    reading_count = 0
+                else:
+                    # if no reading found, up the idle reading count and prompt the user
+                    reading_count += 1
+                    # save the current idle reading
+                    last_idle_reading = reading
+                    print("Waiting for punch: ", reading_count)
+
+                # if enough readings pass without the bag being hit, exit the program.
+                if reading_count > 3:
+                    print("Too slow! Exiting..")
+                    break
