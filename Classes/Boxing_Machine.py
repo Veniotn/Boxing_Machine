e4c97bbbadf6505a127988ff573581bd3cfdfa59
Veniotn <w0404614@nscc.ca>
2023-11-24 19:28:15 -0400

    Calculates force of a punch based off angular velocity

--- Classes/Boxing_Machine.py
+++ Classes/Boxing_Machine.py
@@ -1,5 +1,7 @@
 import math
+import sys
 import time
+
 
 class BoxingMachine:
 
@@ -10,22 +12,12 @@ class BoxingMachine:
         def convert_kg(data):
             return data / 1000
 
-        def calculate_score(reading, last_idle_reading, elapsed_time):
+        def calculate_score(elapsed_time):
             # bags mass, calculated by taking the dimensions of my speed bag to get its volume,
             # and the density of its material. genuine leather sits between,
             # .9 to 1.2 g/cm2, so we used 1.0 g/cm2 as an average density, converted it to cubic meters,
             # we multiply the volume and density together to get our mass in grams and finally convert it to kgs.
             BAG_MASS_KG = 0.023328
-
-            # if either readings are negative, negate them
-            if reading < 0:
-                reading = -reading
-            if last_idle_reading < 0:
-                last_reading = -last_idle_reading
-
-            # our reading is in grams, so we convert it to kgs to stay consistent
-            reading = convert_kg(reading)
-            last_idle_reading = convert_kg(last_reading)
 
             # calculate angular velocity, the bag will travel 90 degrees before it hits the bag
             ANGULAR_DISPLACEMENT = 90
@@ -36,21 +28,25 @@ class BoxingMachine:
             # calculate angular velocity
             angular_velocity = angular_displacement_rad / elapsed_time
 
-
             # calculate acceleration as a = angular_velocity / elapsed_time
             acceleration = angular_velocity / elapsed_time
 
             # calculate force as f = m * a
-            punch_force = BAG_MASS_KG * acceleration
+            punch_force = (BAG_MASS_KG * acceleration)
             print("Score: ", punch_force)
+
+        def prompt_user(this):
+            choice = input("Press E to exit or any other key to punch again")
+
+            if choice.lower() == 'e':
+                print("Exiting...")
+                sys.exit()
 
         def run(this):
             # idle limit is the max value a reading can be before we register it as a valid hit
             IDLE_LIMIT = 3000
             # amount of readings since last valid reading
             reading_count = 0
-            # value of the last idle reading used to find the value of our hit.
-            last_idle_reading = 0
 
             # prompt the user to interact.
             print("PUNCH TO GET A HIGH SCORE!")
@@ -68,14 +64,14 @@ class BoxingMachine:
                     # calculate the elapsed time since the bag has been punched
                     elapsed_time = time.time() - start_time
                     # calculate our score
-                    this.calculate_score(reading, last_idle_reading, elapsed_time)
+                    this.calculate_score(elapsed_time)
+                    # prompt the user if they want to hit the bag again
+                    this.prompt_user()
                     # reset idle reading count
                     reading_count = 0
                 else:
                     # if no reading found, up the idle reading count and prompt the user
                     reading_count += 1
-                    # save the current idle reading
-                    last_idle_reading = reading
                     print("Waiting for punch: ", reading_count)
 
                 # if enough readings pass without the bag being hit, exit the program.
